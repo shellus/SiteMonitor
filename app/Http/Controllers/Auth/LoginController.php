e5129af0af05928c703daf777d166c6fc410dd2f
shellus <shellus@endaosi.com>
2018-01-15 11:46:47 +0800

    修改登录方式为用户名登录

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,9 @@ class LoginController extends Controller
     	$this->redirectTo=route('monitor.index');
         $this->middleware('guest')->except('logout');
     }
+
+    public function username()
+    {
+        return 'name';
+    }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,15 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">UserName</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('name'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                             </div>
